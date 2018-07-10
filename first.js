06e2de8f7d2b888dbd2eeaad5a819dbd29c9de05
muhammed burak şentürk <buraksenturk25@gmail.com>
2018-07-10 08:06:44 +0300

    Update first.js

--- first.js
+++ first.js
@@ -7,7 +7,7 @@ import {
   Text,
   Button
 } from "react-native";
-import { BleManager, Device, Uuid } from "react-native-ble-plx";
+import { BleManager } from "react-native-ble-plx";
 import DeviceList from "./app/deviceList";
 import { fromByteArray, toByteArray } from "base64-js";
 const postData = [
@@ -34,13 +34,69 @@ export default class App extends Component {
       selectDeviceData: {},
       isDeviceConnected: false,
       bleStatus: false,
-      id: "99:99:01:00:10:90",
-      getMessage: []
+      writed: false,
+      id: "06:73:01:00:10:90", //"99:99:01:00:10:90",
+      getMessage: [],
+      log: {}
     };
     this.scanAndConnect = this.scanAndConnect.bind(this);
     this.disconnect = this.disconnect.bind(this);
+    setInterval(() => {
+      this.controller();
+    }, 1000);
   }
-
+  logHandler(type, text, status, deviceWrited) {
+    let log = {};
+    log[new Date().getTime()] = {
+      type: type,
+      text: text,
+      status: status,
+      deviceWrited: deviceWrited
+    };
+    this.setState({ log: log });
+  }
+  controller() {
+    this.manager
+      .isDeviceConnected(this.state.id)
+      .then(v => {
+        console.log(v);
+        if (v) {
+          this.logHandler(
+            "info",
+            "Cihaz ile bağlantı kuruldu",
+            "success",
+            this.state.writed
+          );
+          if (this.state.writed) {
+            this.logHandler(
+              "info",
+              "Cihaza veriler başarılı bir şekilde yazıldı",
+              "success",
+              this.state.writed
+            );
+          }
+        } else if (this.state.writed) {
+          this.logHandler(
+            "info",
+            "Cihaza veriler başarılı bir şekilde yazıldı",
+            "success",
+            this.state.writed
+          );
+        } else if (v === false) {
+          this.logHandler(
+            "warning",
+            "Cihaz ile olan bağlantı koptu",
+            "disconnect",
+            this.state.writed
+          );
+          this.scanAndConnect();
+        }
+        this.setState({ isDeviceConnected: false, writed: false });
+      })
+      .catch(e => {
+        this.logHandler("error", e, "error", null);
+      });
+  }
   componentDidMount() {
     if (Platform.OS === "android" && Platform.Version >= 23) {
       PermissionsAndroid.check(
@@ -63,20 +119,40 @@ export default class App extends Component {
   }
   toHexString(type, byteData) {
     let s = "0x";
-    const byteArray = toByteArray(byteData);
+    let byteArray = toByteArray(byteData);
     const { getMessage } = this.state;
-    const hexByte = [];
+    let hexByte = [];
     byteArray.forEach(byte => {
       s = ("0" + (byte & 0xff).toString(16)).slice(-2);
       hexByte.push(s);
     });
+
     getMessage[type] = hexByte;
     this.setState({ getMessage: Object.assign(getMessage, getMessage) });
     console.log(this.state.getMessage);
   }
+  checsumControl(byteData) {
+    console.log(typeof byteData, byteData);
+    let x = 0,
+      s = "0x";
+    for (let i = 1; i < 10 - 2; i++) {
+      x += byteData[i];
+    }
+    byteData[10] = x;
+    console.log(byteData);
+    const hexByte = [];
+    byteData.forEach(byte => {
+      s = ("0" + (byte & 0xff).toString(16)).slice(-2);
+      hexByte.push(s);
+    });
+    console.log(hexByte);
+    return hexByte;
+  }
+  messageTypeController(type, data) {}
   async write(device, service, char, data) {
     let secondData = postData,
       thirdData = postData;
+
     const characteristic = await this.manager.writeCharacteristicWithoutResponseForDevice(
       device.id,
       service,
@@ -84,39 +160,39 @@ export default class App extends Component {
       fromByteArray(data)
     );
     const firstResp = characteristic.read();
+
     firstResp.then(async v => {
-      this.toHexString("0xa1", v.value);
-      let secondData = postData;
       secondData[3] = 0xa2;
-      if (this.state.getMessage["0xa1"]) {
-        const secondCharacteristic = await this.manager.writeCharacteristicWithoutResponseForDevice(
-          device.id,
-          service,
-          char,
-          fromByteArray(secondData)
-        );
-        const secondResp = secondCharacteristic.read();
-        secondResp.then(v => {
-          console.log("0xa2", toByteArray(v.value));
-          this.toHexString("0xa2", v.value);
-          let thirdData = postData;
-          thirdData[3] = 0xa5;
-        });
-      }
-      if (this.state.getMessage["0xa1"] && this.state.getMessage["0xa2"]) {
-        const thirdCharacteristic = await this.manager.writeCharacteristicWithoutResponseForDevice(
-          device.id,
-          service,
-          char,
-          fromByteArray(thirdData)
-        );
-        const thirdResp = thirdCharacteristic.read();
-        thirdResp.then(async v => {
-          console.log("0xa5", toByteArray(v.value));
-          this.toHexString("0xa5", v.value);
-        });
-      }
+      secondData = this.checsumControl(secondData);
+      await this.toHexString("0xa1", v.value);
+      const secondCharacteristic = await this.manager.writeCharacteristicWithoutResponseForDevice(
+        device.id,
+        service,
+        char,
+        fromByteArray(secondData)
+      );
+      const secondResp = secondCharacteristic.read();
+      await secondResp.then(v => {
+        thirdData[3] = 0xa5;
+        thirdData = this.checsumControl(thirdData);
+        this.toHexString("0xa2", v.value);
+      });
+      const thirdCharacteristic = await this.manager.writeCharacteristicWithoutResponseForDevice(
+        device.id,
+        service,
+        char,
+        fromByteArray(thirdData)
+      );
+
+      const thirdResp = thirdCharacteristic.read();
+      console.log(thirdCharacteristic);
+      await thirdResp.then(async v => {
+        console.log(thirdData[3], toByteArray(v.value));
+        await this.toHexString("0xa5", v.value);
+      });
+      this.setState({ writed: true });
     });
+    // console.log(this.state.getMessage);
   }
   /*
         * Cihaz bağlantısını kopar
@@ -137,15 +213,12 @@ export default class App extends Component {
   }
 
   scanAndConnect() {
-    let obj = {},
-      deviceArr = [];
     this.manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
         // Handle error (scanning will be stopped automatically)
         this.setState({ info: "cihazlar bulunamadı", bleStatus: false });
         return false;
       }
-
       if (device.id !== null && device.id === this.state.id) {
         // Creating an array of devices with names
         this.setState({ bleStatus: true });
@@ -161,7 +234,10 @@ export default class App extends Component {
             console.log(device);
             return this.write(device, "aaaa", "bbbb", postData);
           })
-          .catch(error => console.log(error));
+          .catch(e => {
+            this.logHandler("error", e, "error", null);
+            this.setState({ writed: false });
+          });
         this.setState({
           devices: device,
           isDeviceConnected: true
